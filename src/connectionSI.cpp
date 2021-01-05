8f3dd113bbfb42aff23f6f409b2d2840f28b32cd
nicolosh <nicolo.cavalieri@studenti.unitn.it>
2021-01-05 15:45:02 +0100

    commit 2(ok)

--- src/connectionSI.cpp
+++ src/connectionSI.cpp
@@ -5,9 +5,11 @@
 #include "../include/covid/connectionSI.h"
 
 double covid::connectionSI::computeFlow(double dt) {
+    flow_ = 0.0;
     for (int i = 0; i < dependentNodes_coeff.size(); ++i)
     {
         flow_ += dependentNodes_coeff[i].X_->getValue() * dependentNodes_coeff[i].coeff;
     }
-    return (flow_ * src_->getValue() * dt);
+    flow_ = flow_ * src_->getValue() * dt;
+    return flow_;
 }
